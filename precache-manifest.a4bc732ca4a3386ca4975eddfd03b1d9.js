a5f561a90f726c279d437eef16c97630f0939b30
Kamil Mackow <kamilmackow98@gmail.com>
2020-04-24 16:45:22 +0200

    Updates

---
+++ precache-manifest.a4bc732ca4a3386ca4975eddfd03b1d9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aee7e04f5edb65f4fa9bae4bc16bda",
+    "url": "/player/index.html"
+  },
+  {
+    "revision": "f918bd17d74815b9f442",
+    "url": "/player/static/css/main.a8b12aea.chunk.css"
+  },
+  {
+    "revision": "0006ba18cfb8cee72b21",
+    "url": "/player/static/js/2.9f042dd2.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/player/static/js/2.9f042dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f918bd17d74815b9f442",
+    "url": "/player/static/js/main.044412de.chunk.js"
+  },
+  {
+    "revision": "8beab540b4781a9344ea",
+    "url": "/player/static/js/runtime-main.f696826a.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/player/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/player/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/player/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/player/static/media/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/player/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/player/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/player/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2bc32258433ce03d56ab61261d46d893",
+    "url": "/player/static/media/Saira-Medium.2bc32258.ttf"
+  },
+  {
+    "revision": "64c09bfd13be208a458423d377e2512d",
+    "url": "/player/static/media/Saira-Regular.64c09bfd.ttf"
+  },
+  {
+    "revision": "43368c2b5c05e7f13f9a308223cba8fe",
+    "url": "/player/static/media/Saira-SemiBold.43368c2b.ttf"
+  },
+  {
+    "revision": "1fdb979840ea3e3d9505a2423ccb3e5e",
+    "url": "/player/static/media/unknown.1fdb9798.png"
+  }
+]);
